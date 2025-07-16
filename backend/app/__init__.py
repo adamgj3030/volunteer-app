88c1d9939e5ab68bb2b7cff1db0f3c8e527effaf
Abyss999 <abusivehydro@gmail.com>
2025-07-16 13:36:48 -0500

    added back config

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,9 @@
 from app.imports import *
 # from app.database import db  
 migrate = Migrate()
-def create_app():
+def create_app(config_object="app.config.DevConfig"):
     app = Flask(__name__)
-    CORS(app)
+    app.config.from_object(config_object)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST", "localhost")}:{os.getenv("DB_PORT", "5432")}/{os.getenv("DB_NAME")}"
@@ -12,6 +12,7 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default-key")
 
     db.init_app(app)
+    CORS(app)
 
     for blueprint, prefix in blueprint_with_prefixes.items():
         app.register_blueprint(blueprint, url_prefix=prefix)
@@ -21,5 +22,7 @@ def create_app():
     # with app.app_context():
     #     from app import models
     #     db.create_all()
+
+    return app
 
 __all__ = ["create_app", "db"]
